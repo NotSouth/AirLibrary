442bcfa5cde189412123d31be92fc21a55542e8a
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-11-23 09:31:23 +0100

    Add project files.

---
+++ AirLibrary/Air.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AirLibrary
+{
+    /// <summary>
+    /// Class to hold all the data gathered from the sensor
+    /// </summary>
+    public class Air
+    {
+        //properties are strings because we can always parse them later in the case they are numbers
+        //i dont know how the sensor will display this data
+        public string Temperature { get; set; }
+        public string CO2 { get; set; }
+        public string Humidity { get; set; }
+
+        public Air(string temp, string co2, string humidity)
+        {
+            Temperature = temp;
+            CO2 = co2;
+            Humidity = humidity;
+        }
+        public Air()
+        {
+
+        }
+    }
+}
