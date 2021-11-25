48fe6c7dd87de7db99843ee899efe1226913b92f
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-11-25 12:14:26 +0100

    Changed properties from string to double

--- AirLibrary/Air.cs
+++ AirLibrary/Air.cs
@@ -10,12 +10,12 @@ namespace AirLibrary
         //properties are strings because we can always parse them later in the case they are numbers
         //i dont know how the sensor will display this data
         public int ID { get; set; }
-        public string Temperature { get; set; }
-        public string CO2 { get; set; }
-        public string Humidity { get; set; }
+        public double Temperature { get; set; }
+        public double CO2 { get; set; }
+        public double Humidity { get; set; }
         public DateTime TimeStamp { get; set; }
 
-        public Air(string temp, string co2, string humidity)
+        public Air(double temp, double co2, double humidity)
         {
             TimeStamp = DateTime.Now;
             Temperature = temp;
